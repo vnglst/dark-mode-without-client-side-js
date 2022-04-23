0f56aae61c5f7193260730fb38b5050288e88fd2
Koen van Gilst <koen@koenvangilst.nl>
2022-04-23 22:39:04 +0200

    cleanup

--- App.tsx
+++ App.tsx
@@ -3,14 +3,16 @@
 import { h } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
 
 type Props = {
-  mode: string;
+  mode?: string;
 };
 
 export function App({ mode }: Props) {
   return (
-    <html class={mode}>
-      <link rel="stylesheet" href="/style.css" />
+    <html class={mode ? (mode === "dark" ? "dark" : "light") : ""}>
       <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/style.css" />
         <title>Hello darkness mode</title>
       </head>
       <body>
@@ -23,7 +25,7 @@ export function App({ mode }: Props) {
           </form>
         </main>
       </body>
-      <script src="/script.js" />
+      <script src="/script.js"></script>
     </html>
   );
 }
