4fd9fc8bd25165c43c2d86317680977e505a9b5a
Koen van Gilst <koen@koenvangilst.nl>
2022-04-24 11:29:45 +0200

    fix: better focus management

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ lightForm.onsubmit = function (e) {
   e.preventDefault();
   fetch(lightForm.action, { method: "POST" });
   document.documentElement.className = "dark";
+  document.getElementById("dark-button").focus();
 };
 
 darkForm.onsubmit = function (e) {
   e.preventDefault();
   fetch(darkForm.action, { method: "POST" });
   document.documentElement.className = "light";
+  document.getElementById("light-button").focus();
 };
